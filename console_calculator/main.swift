1581ef4484cc4b560d599b02a24ceca223e33533
Artem Kriukov <art.kriukov@gmail.com>
2024-07-14 23:24:47 +0300

    feat: Implemented a simple calculator

--- console_calculator/main.swift
+++ console_calculator/main.swift
@@ -1,11 +1,33 @@
-//
-//  main.swift
-//  console_calculator
-//
-//  Created by Artem on 7/14/24.
-//
+print("Добро пожаловать в программу калькулятор")
 
-import Foundation
+print("Выберите операцию: +, -, * или /")
+let operation = readLine() ?? ""
 
-print("Hello, World!")
+print("Введите целое число")
+let firstNum = readLine() ?? ""
 
+print("Введите второе целое число")
+let secondNum = readLine() ?? ""
+
+print("Идет вычисление примера: " + firstNum + " " + operation + " " + secondNum)
+
+if let firstNum = Int(firstNum){
+    if let secondNum = Int(secondNum){
+        switch operation {
+        case "+": print("Результат: " + String(firstNum + secondNum))
+        case "-": print("Результат: " + String(firstNum - secondNum))
+        case "*": print("Результат: " + String(firstNum * secondNum))
+        case "/":
+            if secondNum != 0 {
+                print("Результат: " + String(firstNum + secondNum))
+            }else {
+             print("Деление на 0 является недопустимой операцией")
+            }
+        default: print("Вы ввели неверную операцию")
+        }
+    } else{
+        print("Вы ввели неверно второе число")
+    }
+}else{
+    print("Вы ввели неверно первое число")
+}

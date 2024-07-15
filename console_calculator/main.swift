6fd8c2e9c0162f23ccf47de4fd0115c561e12c74
Artem Kriukov <art.kriukov@gmail.com>
2024-07-15 10:25:18 +0300

    improvement: Added functions getDataFromUser, showResult, colculate. The basic logic has been rewritten into an infinite loop. Added the ability to end the program by pressing "q", and view the history of operations by pressing "h"

--- console_calculator/main.swift
+++ console_calculator/main.swift
@@ -1,33 +1,67 @@
-print("Добро пожаловать в программу калькулятор")
+print("Добро пожаловать в  калькулятор!")
 
-print("Выберите операцию: +, -, * или /")
-let operation = readLine() ?? ""
+var history: [String] = []
 
-print("Введите целое число")
-let firstNum = readLine() ?? ""
-
-print("Введите второе целое число")
-let secondNum = readLine() ?? ""
-
-print("Идет вычисление примера: " + firstNum + " " + operation + " " + secondNum)
-
-if let firstNum = Int(firstNum){
-    if let secondNum = Int(secondNum){
-        switch operation {
-        case "+": print("Результат: " + String(firstNum + secondNum))
-        case "-": print("Результат: " + String(firstNum - secondNum))
-        case "*": print("Результат: " + String(firstNum * secondNum))
-        case "/":
-            if secondNum != 0 {
-                print("Результат: " + String(firstNum + secondNum))
-            }else {
-             print("Деление на 0 является недопустимой операцией")
-            }
-        default: print("Вы ввели неверную операцию")
+while true {
+    let operation = getDataFromUser(description: "Выберите операцию: +, -, * или /. Для завершения работы нажмите q. Для просмотра истории нажмите h." )
+    
+    if operation == "q"{
+        exit(0)
+    } else if operation == "h"{
+        for example in history {
+            print(example)
         }
-    } else{
-        print("Вы ввели неверно второе число")
     }
-}else{
-    print("Вы ввели неверно первое число")
+    
+    let firstNum = getDataFromUser(description: "Введите первое число")
+    let secondNum = getDataFromUser(description: "Введите второе число")
+    
+    let example = firstNum + " " + operation + " " + secondNum
+
+    print("Программа делает вычисление примера: " + example)
+
+    if let firstNumber = Int(firstNum) {
+        if let secondNumber = Int(secondNum) {
+            let result = colculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
+            if let result = result {
+                showResult(result)
+                history.append(example + " = " + String(result))
+            }
+        }else {
+            print("Вы ввели неверное число")
+        }
+    }else{
+        print("Вы ввели неверное число")
+    }
+    
+    print("")
+    print("------------------------")
+    print("")
 }
+
+func getDataFromUser(description: String) -> String{
+    print(description)
+    return readLine() ?? ""
+}
+
+func showResult(_ result: Int){
+    print("Результат: " + String(result))
+}
+
+func colculate(operation: String, firstNumber: Int, secondNumber: Int ) -> Int? {
+    switch operation{
+    case "+" : return firstNumber + secondNumber
+    case "-" : return firstNumber - secondNumber
+    case "*" : return firstNumber * secondNumber
+    case "/" :
+        if secondNumber != 0 {
+            return firstNumber / secondNumber
+        }else{
+            print("Деление на 0 запрещенно")
+            return nil
+        }
+    default: print("Вы ввели неверную операцию")
+        return nil
+    }
+}
+
